17e0cc3b9b11237e3b4e1ad5d2c252807ee29eaf
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-08-18 20:48:13 +0530

    Done with design part

--- App.js
+++ App.js
@@ -52,10 +52,30 @@ componentDidMount(){
 
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Welcome to app</Text>
+      <KeyboardAvoidingView behavior='padding' enabled style={styles.container}>
+        <View style={styles.header}>
+        <Text style={styles.headerText}>Message Board</Text>
+        </View>
+        <View style={styles.listContainer}>
+        <FlatList data={this.state.msglst} inverted
+          renderItem={({item})=>(
+            <Card style={styles.listItem}>
+              <Text style={styles.messageText}>{item.text}</Text>
+              <Text style={styles.timeText}>
+                {new Date(item.time).toLocaleDateString}</Text>
+            </Card>
+          )} keyExtractor={a=>a.time.toString()}/>
+          </View>
+          <View style={styles.inputContainer}>
+            <Input onChangeText={msg=>this.setState({msg})}
+              value={this.state.msg} placeholder='Enter message'/>
+            <Button danger rounded icon
+              onPress={()=>this.sendmsg(this.state.msg)}>
+              <Icon name='arrow-forward'/>
+            </Button>
+          </View>
         <StatusBar style="auto" />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -64,8 +84,40 @@ componentDidMount(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    margin: 2,
+    backgroundColor: "#01CBC6"
   },
+  header: {
+    backgroundColor: "#2B2B52",
+    alignItems: "center",
+    height: 40,
+    justifyContent: "center"
+  },
+  headerText: {
+    paddingHorizontal: 10,
+    color: "#FFF",
+    fontSize: 20
+  },
+  listContainer: {
+    flex: 1,
+    padding: 5
+  },
+  listItem: {
+    padding: 10
+  },
+  messageText: {
+    fontSize: 20
+  },
+  timeText: {
+    fontSize: 10
+  },
+  inputContainer: {
+    flexDirection: "row",
+    padding: 5,
+    borderWidth: 5,
+    borderRadius: 15,
+    borderColor: "#2B2B52",
+    color: "#fff"
+  }
 });
